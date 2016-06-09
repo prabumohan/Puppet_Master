dcf3670448fd28b8f812183e31c949194614bd77
prabumohan <prabu.mohan@outlook.com>
2016-06-09 14:03:53 +0100

    Updated apache config

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,5 +5,12 @@ node 'node1' {
     docroot => '/var/www/html'
   }
 }
+node 'lamp-1' {
+  class { 'apache': }             # use apache module
+  apache::vhost { 'www.example.com':  # define vhost resource
+    port    => '80',
+    docroot => '/var/www/html'
+  }
+}
 
 # node default {}       # uncomment this line if it doesn't already exist in your manifest
