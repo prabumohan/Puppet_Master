bd4c3795af16c60a43b3104646ae6cde9e291c42
prabumohan <prabu.mohan@outlook.com>
2016-06-09 11:50:11 +0100

    done
    done

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,5 @@
 file {'/tmp/example-ip':                                            # resource type file and filename
   ensure  => present,                                               # make sure it exists
   mode    => '0644',                                                # file permissions
-  content => "Ruby version ${serialnumber}.\n",  # note the ipaddress_eth0 fact
   content => "Ruby version ${puppetversion}.\n",  # note the ipaddress_eth0 fact
 }
