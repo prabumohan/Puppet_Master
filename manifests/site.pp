b1dad883d863dc99a49d7bfdcd3c50c2e234ccd4
prabumohan <prabu.mohan@outlook.com>
2016-06-09 14:20:02 +0100

    updated hostname

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,7 +5,7 @@ node 'node1' {
     docroot => '/var/www/html'
   }
 }
-node 'lamp-1.localdomain' {
+node 'lamp-1' {
   class { 'apache': }             # use apache module
   apache::vhost { 'www.example.com':  # define vhost resource
     port    => '80',
